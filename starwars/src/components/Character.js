00d57281778f5a633abff4a33e3afb12bc9c477a
Ronny Alvarado <rsalvarado777@gmail.com>
2019-03-09 11:31:38 -0600

    created the Character.js file to return each character to display on screen

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Character (props) {
+    return (
+        <div className="CharacterCard">
+            <h2>Name: {props.eachChar.name} </h2>
+            <h2>Gender: {props.eachChar.gender}</h2>
+            <h2>Height: {props.eachChar.height}</h2>
+            <h2>Hair Color: {props.eachChar.hair_color}</h2>
+        </div>
+    )
+}
+export default Character;
